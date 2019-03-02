e7ae172fbd7c86a1856d4c8c4f2776de99bc64de
go4retro <go4retro@go4retro.com>
2019-03-01 20:30:15 -0600

    Moved internal RAM back to banks 0-7, and added code to override $fffx vectors into bank $3f and read them from there.

--- hdl/CocoMemJr.v
+++ hdl/CocoMemJr.v
@@ -53,17 +53,22 @@ assign ce_access_dat_hi =              address_cpu == 16'hfd05;
 
 wire ce_fxxx;
 wire ce_ffxx;
+wire ce_vector;
+wire ce_alt_vector;
 wire ce_fexx;
+wire ce_crm;
 wire ce_mmu_regs;
 wire ce_init0;
 wire ce_init1;
 wire ce_task_hi;
+wire ce_mem_template;
 wire ce_mem;
 wire we_mem;
 wire ce_dat;
 wire flag_mmu_enabled;
 wire flag_crm_enabled;
 wire flag_alt_regs;
+//wire flag_alt_vector;
 wire flag_ext_mmu;
 wire [11:0]dat_task_active; // 5 bits of task number in init1 register + 7 bits of tasklets in another register.
 wire [11:0]dat_task_access;   // used to select "window" of task values to show in $ffax
@@ -76,6 +81,8 @@ reg [14:0]address_dat_out;
 
 assign ce_fxxx =                       (address_cpu[15:9] == 7'b1111111);
 assign ce_ffxx =                       ce_fxxx & (address_cpu[8] == 1);
+assign ce_vector =                     ce_ffxx & (address_cpu[7:4] == 4'b1111);
+assign ce_alt_vector =                 ce_vector & flag_crm_enabled;
 assign ce_fexx =                       ce_fxxx & (address_cpu[8] == 0);
 assign ce_mmu_regs =                   ce_ffxx & (address_cpu[7:4] == 4'h9);
 assign ce_init0 =                      ce_mmu_regs & (address_cpu[3:0] == 4'h0);
@@ -83,14 +90,39 @@ assign ce_init1 =                      ce_mmu_regs & (address_cpu[3:0] == 4'h1);
 assign ce_task_hi =                    ce_mmu_regs & (address_cpu[3:0] == 4'h7);
 assign ce_dat =                        ce_ffxx & (address_cpu[7:4] == 4'ha);
 assign ce_crm =                        flag_crm_enabled & ce_fexx;
-assign ce_mem =                        !ce_ffxx & flag_mmu_enabled & (address_out[20:16] != 5'b00111);   // hole at bank 38-3f for main memory
-assign we_mem =                        ce_mem & !r_w_cpu;
+assign ce_mem_template =               flag_mmu_enabled & (address_out[20:16] != 5'b0);   // hole at bank 00-07 for main memory
+assign ce_mem =                        (ce_alt_vector | !ce_ffxx) & ce_mem_template;      // mmu on and (crm & vectors or anything but IO) 
+// writes to vectors bleed through, when mmu on, even if crm not on.  This allows one to change vectors before making them active.
+assign we_mem =                        ((ce_vector & flag_mmu_enabled) | !ce_ffxx) & ce_mem_template & !r_w_cpu;
 assign we_dat_l =                      (e & !r_w_cpu & ce_dat & (!flag_alt_regs | (flag_alt_regs & !flag_ext_mmu) | (flag_ext_mmu & address_cpu[0])));
 assign we_dat_h =                      (e & !r_w_cpu & ce_dat & (flag_ext_mmu & !address_cpu[0]));
 
 `ifdef TESTING
 assign address_mem =                   (ce_access_mem ? addr : {address_out[20:13], address_cpu[12:0]});
 `else
+/*
+   The Color Computer 3 has the $fffx vectors jump through $fefx to their final destination, as a way 
+   to suport remapping the vectors (normally, regardless of SAM mode, $fffx is pulled from ROM)
+   The Coco1/2 vectors jump through low memory, because some Cocos were not 64K machines, so low
+   memory was the only sure place to send the vectors.
+   
+   In my opinion, the best way to handle this is to open a "hole" in $fffx when the MMU is on to allow
+   modifying the vectors.  However, Tormod Volden's MOOH card operates slightly differently, It maps 
+   the crm page to $ffxx, and also mirros it to $fexx.  
+   
+   I have a few concerns about this:
+   
+   * It commits the $fefx space to vectors.  I suppose that's not horrible, as CoCo 3 mandates that as
+     well, but I'm not a fan of such a requirement
+   * It requires additional fixups on the high byte of the address ($ff -> $fe).  Not horrid, but added code
+   * It modifies the jump table at the top of CRM.  Coco3 apps might expect the table to be 3 byte
+     entries, like the CoCo 3 requires, but the MOOH is a direct map to the original vectors, which 
+     are 2 byte entries.  I don't like the mixing of sizes
+   
+   I think the $fffx mapping should simply open a hole in RAM at that location.  If CRM is on, then select
+   $3f like with CRM.  But, if it is off, let whatever bank of memory that resides at the upper 8K bleed through.   
+   
+*/
 assign address_mem =                   {address_out[20:13], address_cpu[12:0]};
 `endif
 assign address_brd[15:13] =            address_out[15:13];
@@ -126,31 +158,31 @@ register #(.WIDTH(5))                  reg_addrh(e, !_reset, ce_test_high & !r_w
 always @(*)
 begin
 `ifdef TESTING
-   if(ce_access_dat_lo | ce_access_dat_hi)   // test code
+   if(ce_access_dat_lo | ce_access_dat_hi)      // test code
       address_dat_out = addr[14:0];
    else 
 `endif
-   if(ce_dat & !flag_ext_mmu)       // DAT register access
+   if(ce_dat & !flag_ext_mmu)                   // DAT register access
       address_dat_out = {dat_task_access,3'b0} + address_cpu[3:0];
-   else if(ce_dat & flag_ext_mmu)        // Extended DAT register access
+   else if(ce_dat & flag_ext_mmu)               // Extended DAT register access
       address_dat_out = {dat_task_access, address_cpu[3:1]};
-   else                                      // DAT MMU usage
+   else                                         // DAT MMU usage
       address_dat_out = {dat_task_active, address_cpu[15:13]};
 end
 
 always @(*)
 begin
-   if(ce_init0)                              // if accessing mmu register init0
+   if(ce_init0)                                 // if accessing mmu register init0
       data_out = {0,flag_mmu_enabled,2'b0,flag_crm_enabled,3'b0};
-   else if(ce_init1 & !flag_alt_regs)         // read active task low bits
+   else if(ce_init1 & !flag_alt_regs)           // read active task low bits
       data_out = {3'b0, dat_task_active[4:0]}; 
-   else if(ce_init1 & flag_alt_regs)          // read windowed task low bits
+   else if(ce_init1 & flag_alt_regs)            // read windowed task low bits
       data_out = {'b1, flag_ext_mmu, 'b0, dat_task_access[4:0]}; 
-   else if(ce_task_hi & !flag_alt_regs)       // read active task high bits
+   else if(ce_task_hi & !flag_alt_regs)         // read active task high bits
       data_out = {'b0, dat_task_active[11:5]}; 
-   else if(ce_task_hi & flag_alt_regs)        // read windowed task high bits
+   else if(ce_task_hi & flag_alt_regs)          // read windowed task high bits
       data_out = {'b0, dat_task_access[11:5]}; 
-   else if(ce_dat & !flag_ext_mmu)       // read MMU task regs low bytes
+   else if(ce_dat & !flag_ext_mmu)              // read MMU task regs low bytes
       data_out = data_dat[7:0];
    else if(ce_dat & flag_ext_mmu & !address_cpu[0]) // high byte
       data_out = data_dat[15:8];
@@ -182,12 +214,22 @@ begin
    if(ce_access_mem)
       address_out[20:13] = {5'b0,addr[15:13]};
    else
-`endif   
-   if (ce_crm)                       // if CRM enabled, and $fexx access, pin bank to $3f
-    address_out = 8'h3f;
-   else if(flag_mmu_enabled & !ce_ffxx)   // if we're in MMU and not asking for IO page, use DAT
+`endif
+/* 
+   if we're reading or writing in CRM page with CRM on
+   or we're reading vectors with CRM on
+   or we're writing vectors with MMU on
+   pin page to $3f
+   
+   We can probably just do crm | vector, as $3f will get trimmed to
+   $7 for a[15:13], which is what the original CPU is sending anyway
+ */
+
+   if (ce_crm | ce_alt_vector | (!r_w_cpu & ce_vector & flag_mmu_enabled))
+    address_out[20:13] = 8'h3f;
+   else if(flag_mmu_enabled & !ce_ffxx)         // if we're in MMU and not asking for IO page, use DAT
       address_out[20:13] = data_dat[7:0];
-   else                                   // otherwise, pass through upper 3 bits.
+   else                                         // otherwise, pass through upper 3 bits.
       address_out[20:13] = {5'b0,address_cpu[15:13]};
 end
 
